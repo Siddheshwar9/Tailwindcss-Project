6e3d0e81706743c4ca71791d5ba014362a0d4023
Siddheshwar9 <siddeshwarpandey123@gmail.com>
2025-08-30 02:17:05 +0530

    Feat: some modification in the index and package.json
    Signed-off-by: Siddheshwar9 <siddeshwarpandey123@gmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
   theme: {
     extend: {
       colors: {
@@ -13,4 +16,3 @@ module.exports = {
   },
   plugins: [],
 }
-
